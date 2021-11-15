2c4f869691002e0611efb07a4e14af7fa18f8618
quangpn0204 <19522100@gm.uit.edu.vn>
2021-11-15 20:18:15 +0700

    Active record

---
+++ Architectural_Pattern/Architectural_Pattern/ActiveRecord/Product.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architectural_Pattern.ActiveRecord
+{
+    class Product
+    {
+        private Guid guid; // Note that unique ID is read-only!
+        private string productName;
+        private int price;
+        private string description;
+        private string image;
+        private string idBrand;
+        private string idCategory;
+        private int quantity;
+
+        public Product() { }
+        public Product(Guid guid, string name, int price, string description, string image, string idBrand, string idCategory, int quantity)
+        {
+            this.guid = guid;
+            this.productName = name;
+            this.price = price;
+            this.description = description;
+            this.image = image;
+            this.idBrand = idBrand;
+            this.idCategory = idCategory;
+            this.Quantity = quantity;
+        }
+        
+        public Guid Guid { get => guid; set => guid = value; }
+        public string ProductName { get => productName; set => productName = value; }
+        public int Price { get => price; set => price = value; }
+        public string Description { get => description; set => description = value; }
+        public string Image { get => image; set => image = value; }
+        public string IdBrand { get => idBrand; set => idBrand = value; }
+        public string IdCategory { get => idCategory; set => idCategory = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+
+        public void Update()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("");
+                SqlConnection db = null; //connection string here
+                db.Open();
+
+                string statement = "UPDATE `product` SET `productName`=@productName, `price`=@price, `description`=@description, `image`=@image,`idBrand`=@idBrand, `idCategory`=@idCategory, `quantity` =@quantity where `guid`=@guid";
+                SqlCommand command = new(statement, conn);
+                command.Parameters.AddWithValue("@guid", this.Guid);
+                command.Parameters.AddWithValue("@productName", this.productName);
+                command.Parameters.AddWithValue("@price", this.price);
+                command.Parameters.AddWithValue("@description", this.description);
+                command.Parameters.AddWithValue("@image", this.image);
+                command.Parameters.AddWithValue("@idBrand", this.idBrand);
+                command.Parameters.AddWithValue("@idCategory", this.idCategory);
+                command.Parameters.AddWithValue("@quantity", this.quantity);
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured updateing Students to the data source.", e);
+            }
+        }
+        public void Delete()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("");
+                SqlConnection db = null; //connection string here
+                db.Open();
+
+                string statement = "Deelete `product` where `guid`=@guid";
+                SqlCommand command = new(statement, conn);
+                command.Parameters.AddWithValue("@guid", this.Guid);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured updateing Students to the data source.", e);
+            }
+        }
+        public void Insert()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("");
+                SqlConnection db = null; //connection string here
+                db.Open();
+
+                string statement = "INSERT INTO `students`(guid,productName,price, description, image, idBrand, idCategory, quantity) values(@guid,@productName,@price, @description, @image, @idBrand, @idCategory, @quantity)";
+                SqlCommand command = new(statement, conn);
+                command.Parameters.AddWithValue("@guid", this.Guid);
+                command.Parameters.AddWithValue("@productName", this.productName);
+                command.Parameters.AddWithValue("@price", this.price);
+                command.Parameters.AddWithValue("@description", this.description);
+                command.Parameters.AddWithValue("@image", this.image);
+                command.Parameters.AddWithValue("@idCategory", this.idCategory);
+                command.Parameters.AddWithValue("@idBrand", this.idBrand);
+                command.Parameters.AddWithValue("@quantity", this.quantity);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured inserting Students to the data source.", e);
+            }
+        }
+
+        public Product FindByGuId()
+        {
+
+            try
+            {
+                SqlConnection conn = new SqlConnection("");
+                conn.Open();
+
+                string statement = "SELECT * FROM `product` where `guid`=@guid";
+
+                SqlCommand command = new(statement, conn);
+                command.Parameters.AddWithValue("@guid", this.guid);
+                command.ExecuteNonQuery();
+                SqlDataAdapter adapter = new(command);
+
+                DataTable dataTable = new();
+                adapter.Fill(dataTable);
+
+                Guid guid = Guid.Parse(dataTable.Rows[0].ItemArray[0].ToString());
+                string productName = dataTable.Rows[0].ItemArray[1].ToString();
+                int price = int.Parse(dataTable.Rows[0].ItemArray[2].ToString());
+                string description = dataTable.Rows[0].ItemArray[3].ToString();
+                string image = dataTable.Rows[0].ItemArray[4].ToString();
+                string idBrand = dataTable.Rows[0].ItemArray[5].ToString();
+                string idCategory = dataTable.Rows[0].ItemArray[6].ToString();
+                int quantity = int.Parse(dataTable.Rows[0].ItemArray[7].ToString());
+
+                Product product = new Product(guid, productName, price, description, image, idBrand, idCategory, quantity);
+                return product;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured reading Product from the data source.", e);
+            }
+        }
+
+        //logic 
+        public float calculatorVAT(float persent)
+        {
+            return this.price * persent;
+        }
+        public bool checkQuantity()
+        {
+            return quantity > 0;
+        }
+    }
+}
---
+++ Architectural_Pattern/Architectural_Pattern/ActiveRecord/Student.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architectural_Pattern.ActiveRecord
+{
+
+    public class Student
+    {
+        private long id;
+        private string fullName;
+        private string address;
+        private float gpa;
+        private DateTime dateOfBirth;
+
+        //constructor
+        public Student(long id, string name, string address, float gpa, DateTime dateOfBirth)
+        {
+            this.id = id;
+            this.fullName = name;
+            this.address = address;
+            this.gpa = gpa;
+            this.dateOfBirth = dateOfBirth;
+        }
+        public Student() { }
+
+        //Getter , setter
+
+        public long Id { get => id; set => id = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+        public string Address { get => address; set => address = value; }
+        public float GPA { get => gpa; set => gpa = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+
+        //domain logic method
+        public bool checkBirthDate()
+        {
+            if (DateTime.Today.Day == dateOfBirth.Day && DateTime.Today.Month == dateOfBirth.Month)
+                return true;
+            return false;
+        }
+        public int CaculateAge()
+        {
+            int age = DateTime.Now.Year - DateOfBirth.Year;
+            return age;
+        }
+        public int checkGPA()
+        {
+            if (gpa >= 9)
+            {
+                //Xuất sắc
+                return 4;
+            }
+            else if (gpa >= 8 && gpa < 9)
+            {
+                //Giỏi
+                return 3;
+            }
+            else if (gpa >= 6.5 && gpa < 8)
+            {
+                //Khá
+                return 2;
+            }
+            else if (gpa >= 5 && gpa < 6.5)
+            {
+                //Trung bình
+                return 1;
+            }
+            else
+            {
+                //Yếu
+                return 0;
+            }
+        }
+
+        //SQL Operation Methods
+        public void insert()
+        {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.Open();
+                string query = "INSERT INTO Student value(@id, @fullName, @address, @gpa, @dateOfBirth)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", this.Id.ToString());
+                cmd.Parameters.AddWithValue("@fullName", this.FullName);
+                cmd.Parameters.AddWithValue("@address", this.Address);
+                cmd.Parameters.AddWithValue("@gpa", this.GPA);
+                cmd.Parameters.AddWithValue("@dateOfBirth", this.DateOfBirth.ToString());
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+
+                throw new Exception("Error occured reading Students from the data source.", e);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public void update()
+        {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.Open();
+                string query = "Update Student set fullName = @fullName, address= @address, gpa = @gpa, dateOfBirth = @dateOfbBirth where id = @id";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", this.Id.ToString());
+                cmd.Parameters.AddWithValue("@fullName", this.FullName);
+                cmd.Parameters.AddWithValue("@address", this.Address);
+                cmd.Parameters.AddWithValue("@gpa", this.GPA);
+                cmd.Parameters.AddWithValue("@dateOfBirth", this.DateOfBirth.ToString());
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                // We don't want any types which use the Data Mapper
+                // to be coupled to java.sql.SQLException
+                // So instead, we throw a custom DataMapperException 
+                throw new Exception("Error occured reading Students from the data source.", e);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public void delete()
+        {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.Open();
+                string query = "delete from Student where id = @id";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", this.Id.ToString());
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                // We don't want any types which use the Data Mapper
+                // to be coupled to java.sql.SQLException
+                // So instead, we throw a custom DataMapperException 
+                throw new Exception("Error occured reading Students from the data source.", e);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public Student FindById()
+        {
+            Student student = new Student();
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.Open();
+                string query = "select * from Student where id = " + this.Id.ToString();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                student.Id = int.Parse(dt.Rows[0].ItemArray[0].ToString());
+                student.FullName = dt.Rows[0].ItemArray[1].ToString();
+                student.Address = dt.Rows[0].ItemArray[2].ToString();
+                student.GPA = float.Parse(dt.Rows[0].ItemArray[3].ToString());
+                student.DateOfBirth = DateTime.Parse(dt.Rows[0].ItemArray[4].ToString());
+                return student;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+
+}
